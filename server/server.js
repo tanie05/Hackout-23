6608bd062d75e578f14c207039444d0fb33347fc
tanie05 <tanvee.6352@gmail.com>
2023-11-03 20:14:45 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,40 @@
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
+const PORT = 5000
+
+require('dotenv').config()
+
+const MONGOURI = process.env.MONGOURI
+
+mongoose.connect(MONGOURI)
+mongoose.connection.on("connected", () => {
+    console.log("Successfully connected!!!");
+})
+
+mongoose.connection.on("error", (err) => {
+    console.log("Error connecting!!!", err);
+})
+
+
+// require("./Models/UserModel")
+// require("./Models/ClassModel")
+
+// const authRouter = require('./Routes/AuthRoute')
+// const classRouter = require('./Routes/ClassRoutes')
+// const contentRouter = require('./Routes/ContentRoutes')
+// const user = require('./Routes/UserRoutes')
+const cors = require('cors');
+app.use(cors());
+
+app.use(express.json())
+// app.use('/auth', authRouter)
+// app.use('/classes', classRouter)
+// app.use('/contents', contentRouter)
+// app.use('/users', user)
+
+
+
+app.listen(PORT, () => {
+    console.log("Running on port ", PORT);
+})
