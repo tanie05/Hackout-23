6ce2abdcb65ac2dc207f452b25804db99820ee4f
RamneekBrar <rkbrar348@gmail.com>
2023-11-04 11:07:45 +0530

    Styling signin

--- client/src/Pages/SignIn.js
+++ client/src/Pages/SignIn.js
@@ -1,15 +1,43 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom"
-import { NavLink } from "react-router-dom";
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { UserContext } from "../LandingPage";
-// import '../PagesCSS/SignIn.css'
+import { useNavigate, NavLink } from "react-router-dom"
+
+// function Copyright(props) {
+//   return (
+//     <Typography variant="body2" color="text.secondary" align="center" {...props}>
+//       {'Copyright © '}
+//       <Link color="inherit" href="https://mui.com/">
+//         Your Website
+//       </Link>{' '}
+//       {new Date().getFullYear()}
+//       {'.'}
+//     </Typography>
+//   );
+// }
+
+// TODO remove, this demo shouldn't need to reset the theme.
+
+const defaultTheme = createTheme();
 
 export default function SignIn() {
-    const {state, dispatch} = React.useContext(UserContext)
+  const {state, dispatch} = React.useContext(UserContext)
 
     const navigate = useNavigate();
 
-    const [user, setUser] = useState({
+    const [user, setUser] = React.useState({
         email: "",
         password: ""
     })
@@ -27,8 +55,6 @@ export default function SignIn() {
             )
         })
     }
-
-    // console.log(user);
 
     function signIn() {
         fetch("/auth/signin", {
@@ -59,36 +85,86 @@ export default function SignIn() {
         })
     }
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
 
-    return (
-        <div className="signin-form-container">
-            <div className="signin-form">
-            <input
-                className="input--email"
-                type="email"
-                placeholder="E-mail"
-                value={user.email}
-                name="email"
-                onChange={updateUser}
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              onChange={updateUser}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              onChange={updateUser}
+            />
+            {/* <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            /> */}
+            <Button
+              type="submit"
+              onClick={signIn}
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
             >
-            </input>
-            <input
-                className="input--password"
-                type="password"
-                placeholder="Password"
-                value={user.password}
-                name="password"
-                onChange={updateUser}
-            >
-            </input>
-
-            <button type="submit" onClick={signIn} className="submit-btn" >Sign In</button>
-
-            <div className="message">Don't Have an account?
-                <NavLink style={{textDecoration: "none", color: "rgb(8,38,74)"}} to="/auth/signup"> Sign Up</NavLink>
-            </div>
-        </div>
-        </div>
-        
-    )
+              Sign In
+            </Button>
+            <Grid container>
+              {/* <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid> */}
+              <Grid item>
+              <NavLink style={{textDecoration: "none", color: "rgb(8,38,74)"}} to="/auth/signup">Don't have an account? Sign Up</NavLink>
+                {/* <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link> */}
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        {/* <Copyright sx={{ mt: 8, mb: 4 }} /> */}
+      </Container>
+    </ThemeProvider>
+  );
 }
